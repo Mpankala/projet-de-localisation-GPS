75ac98f3f76f432009596b46a14bf1a750ce6797
Mpankala Linner <lmpankala@gmail.com>
2026-06-10 16:40:07 +0100

    modification de dashboard.js pour la localisation de mon phone

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,115 +1,4 @@
-/*
-let map;
-let marker;
-let circle;
 
-const gpsStatus =
-document.getElementById("gps-status");
-
-const lastUpdate =
-document.getElementById("last-update");
-const latitudeElement =
-document.getElementById("latitude");
-
-const longitudeElement =
-document.getElementById("longitude");
-
-const accuracyElement =
-document.getElementById("accuracy");
-if (!navigator.geolocation) {
-    alert("La géolocalisation n'est pas supportée.");
-} else {
-
-    navigator.geolocation.watchPosition(
-
-        (position) => {
-
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            const accuracy = position.coords.accuracy;
-            latitudeElement.textContent =
-`Latitude : ${latitude.toFixed(6)}`;
-
-longitudeElement.textContent =
-`Longitude : ${longitude.toFixed(6)}`;
-
-accuracyElement.textContent =
-`Précision : ${Math.round(accuracy)} m`;
-            console.log("Latitude :", latitude);
-            console.log("Longitude :", longitude);
-            console.log("Précision :", accuracy);
-            gpsStatus.textContent = "🟢 Actif";
-
-lastUpdate.textContent =
-new Date().toLocaleTimeString();
-
-            // Première position
-            if (!map) {
-
-                map = L.map('map').setView(
-                    [latitude, longitude],
-                    15
-                );
-
-                L.tileLayer(
-                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    {
-                        attribution:
-                        '&copy; OpenStreetMap contributors'
-                    }
-                ).addTo(map);
-
-                marker = L.marker(
-                    [latitude, longitude]
-                ).addTo(map);
-
-                circle = L.circle(
-                    [latitude, longitude],
-                    {
-                        radius: accuracy
-                    }
-                ).addTo(map);
-
-            }
-
-            // Mises à jour suivantes
-            else {
-
-                marker.setLatLng(
-                    [latitude, longitude]
-                );
-
-                circle.setLatLng(
-                    [latitude, longitude]
-                );
-
-                circle.setRadius(
-                    accuracy
-                );
-
-                map.setView(
-                    [latitude, longitude]
-                );
-
-            }
-
-        },
-
-        (error) => {
-            console.error(error);
-            gpsStatus.textContent = "🔴 Inactif";
-        },
-
-        {
-            enableHighAccuracy: true,
-            timeout: 10000,
-            maximumAge: 0
-        }
-
-    );
-
-}
-    */
 const mapStyles = {
 
     streets:
@@ -327,6 +216,42 @@ const marker = new maplibregl.Marker({
 })
 .setLngLat([15.2429, -4.2634])
 .addTo(map);
+if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition(
+
+        async (position) => {
+
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+
+            console.log("GPS :", lat, lng);
+
+            map.flyTo({
+                center: [lng, lat],
+                zoom: 17
+            });
+
+            marker.setLngLat([lng, lat]);
+
+            latitude.textContent =
+            `Latitude : ${lat}`;
+
+            longitude.textContent =
+            `Longitude : ${lng}`;
+
+            accuracy.textContent =
+            `Précision : ${Math.round(position.coords.accuracy)} m`;
+
+        },
+
+        (error) => {
+            console.error(error);
+        }
+
+    );
+
+}
 const styleButtons =
 document.querySelectorAll(".style-btn");
 
